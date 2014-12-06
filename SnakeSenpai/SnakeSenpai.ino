83a1663f7330924fd56e50bfebfb16960435c59f
rkam16 <rkam16@punahou.edu>
2014-12-05 15:24:25 -1000

    Finished Snake
    There is hit detection and a death sound.  I think it is done

--- SnakeSenpai/SnakeSenpai.ino
+++ SnakeSenpai/SnakeSenpai.ino
@@ -1,77 +1,162 @@
-#include <MeggyJrSimple.h>
-int xplayer=3; //Cordinates of player dot
-int yplayer=4;
-int direction=0; //Posible values are 0,90,180,270
-int xapple=random(8);
-int yapple=random(8);
-int marker=3;
+#include <MeggyJrSimple.h> // Required code
 
-void setup() {  // run once, when the sketch starts
-  MeggyJrSimpleSetup();
-  Serial.begin(9600);
-}
- 
-/* Draw PLayer
-   Display Slate
-   Check Buttons
-   Update Direction if a button is pressed
-   Update Player based on direction
-   Correct for Wrap
-*/ 
-
-void loop () 
+void setup()               // required code 
 {
-  Serial.print("x is");
-  Serial.print(xplayer);
-  Serial.print("y is");
-  Serial.print(yplayer);
-  Serial.println();
-  
-
- DrawPx(xplayer,yplayer,Red);
- DrawPx(xapple,yapple,Green);
- DisplaySlate();
- delay(100);
- ClearSlate();
- CheckButtonsDown();
- if (Button_Right) direction=90;
- if (Button_Left) direction=270;
- if (Button_Down) direction=180;
- if (Button_Up) direction=0;
- if (direction==0) yplayer++; 
- if (direction==90) xplayer++;
- if (direction==180) yplayer--;
- if (direction==270) xplayer--;
- if(xplayer>7) xplayer=0;
- if(yplayer>7) yplayer=0;
- if(xplayer<0) xplayer=7;
- if(yplayer<0) yplayer=7;
- {if (xapple==xplayer){
-   if(yapple==yplayer){
-     xapple=random(8);
-     yapple=random(8);
-   }
-  } 
- }
- 
- struct Point {
-   int x;
-   int y;
- };
- Point p1={3,4};
- Point p2={4,4};
- Point p3={5,4};
- Point snakeArray[64] = {p1,p2,p3};
- 
+  MeggyJrSimpleSetup();  
+  Serial.begin(9600); 
 }
 
-void loop();
+int direction = 0;        // possible values are 0,90,180,270
+int xapple = random(8);
+int yapple = random (8);
+int marker = 1;
+boolean alive = false;
+
+
+
+
+struct Point
+{
+  int x;
+  int y;
+};
+
+Point p1 = {3,4};
+Point p2 = {4,4};
+Point p3 = {5,4};
+Point p4 = {6,4};
+Point snakeArray[64] = {p1,p2,p3,p4};
+
+/*
+Draw Player - Call drawSnake()
+Display Slate
+Check buttons
+Update direction if buttons are pressed
+Update player based on direction
+Correct for wrapping
+ 
+*/
+void getApple(void){
+  Tone_Start(ToneC5, 50);
+}
 
 void drawSnake()
 {
-  for (int i=0; i<marker; i++)
+  DrawPx(snakeArray[0].x, snakeArray[0].y,Red);
+  
+  for(int i=1; i<marker; i++)
   {
     DrawPx(snakeArray[i].x,snakeArray[i].y,Red);
   }
 }
+
+void updateSnake()
+{  
+  for (int i = marker-1; i > 0; i--)           
+  {
+    snakeArray[i].x =  snakeArray[i-1].x;
+    snakeArray[i].y =  snakeArray[i-1].y;
+  }
+}
+
+
+void loop()
+{
+  drawSnake();
+  DrawPx(xapple,yapple,Green);
+  DisplaySlate();
+  delay(125);
+  ClearSlate();
+  updateSnake();
+  
+  
+  CheckButtonsPress();
  
+  
+  
+  if (Button_Right)
+  {
+    direction = 90;
+  }
+  
+  if (Button_Left)
+  {
+    direction = 270;
+  }
+  
+  if (Button_Up)
+  {
+   direction = 0; 
+  }
+  
+  if (Button_Down)
+  {
+    direction = 180;
+  }
+  if(direction==0)
+    {
+      snakeArray[0].y++;
+    }
+  if(direction==90)
+    {
+      snakeArray[0].x++;
+    }
+  if(direction==180)
+    {
+      snakeArray[0].y--;
+    }
+  if(direction==270)
+    {
+      snakeArray[0].x--;
+    }
+  if(snakeArray[0].x>7)
+    {
+      snakeArray[0].x=0;
+    }
+  if(snakeArray[0].y>7)
+    {
+      snakeArray[0].y=0;
+    }
+  if(snakeArray[0].x<0)
+    {
+      snakeArray[0].x=7;
+    }
+  if(snakeArray[0].y<0)
+    {
+      snakeArray[0].y=7;
+    }
+  
+    
+    if(xapple == snakeArray[0].x)
+    {
+      if(yapple == snakeArray[0].y)
+      {
+        xapple = random(8);
+        yapple = random(8);
+        marker++;
+        getApple();
+      }
+    }
+    
+      for(int i=1; i<marker; i++)
+      {
+        if(snakeArray[0].x == snakeArray[i].x && snakeArray[0].y == snakeArray[i].y)
+        {
+          alive = true;
+        }
+      } 
+      
+    if(alive)
+    {
+      ClearSlate();                 
+
+      
+      Tone_Start(ToneF3, 1000);
+      DisplaySlate();
+      delay (1000);
+      marker = 1;
+      alive = false;
+      
+      
+    }
+}
